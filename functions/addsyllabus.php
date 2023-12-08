900eb94e75f9580af3bd9893d64c590e8928a6d4
Zhieve12 <145948828+krstngdm@users.noreply.github.com>
2023-12-08 23:53:38 +0800

    Update addsyllabus.php

---
+++ functions/addsyllabus.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <title>Add Syllabus</title>
+    </head>
+    <body>
+        <?php
+            include_once("../dbconnection/mysqlconfig_connection.php");
+
+            if(isset($_POST['submit'])){
+                $code= $_POST['code'];
+                $author= $_POST['author'];
+                $subject= $_POST['subject'];
+
+            
+                if(empty($code) || empty($author)){
+                    if(empty($code)){
+                        echo "<font color='red'>Syllabus Code field is empty</font/><br/>";
+                    }
+                    if(empty($author)){
+                        echo "<font color='red'>Syllabus Author field is empty</font><br/>";
+                    }
+                }
+                else{
+                    $result = mysqli_query($dbc,"<<UPDATE SQL QUERY>>");
+                    echo "<font color='green'>Data added successfully.</font>";
+                    echo "<br/><a href='../index.php>View Result</a>'";
+                }
+            }
+
+        ?>
+    </body>
+</html>
